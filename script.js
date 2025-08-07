701499a0f55d47f4b23ac0b480b49ce1e71141ef
UtkarshloneyaITG <UtkarshLoniya>
2025-08-07 19:17:11 +0530

    to do list added

--- script.js
+++ script.js
@@ -216,10 +216,78 @@ function clearText() {
 let todolistbox = document.getElementById('todolist')
 let todolistinputvalue = document.getElementById('to-do-list-input')
 function todolistadd(){
+  if(todolistinputvalue.value == ''){
+    todolistinputvalue.value = 'Empty'
+  }
   let newlist = document.createElement('li')
-  newlist.innerHTML = `${todolistinputvalue.value}<button onclick='todolistdelete(this)'>Delete</button>`
+  newlist.innerHTML = `${todolistinputvalue.value}<button onclick='todolistdelete(this)' class='todolist-delete-button'>Delete</button>`
   todolistbox.appendChild(newlist)
+  todolistinputvalue.value = ''
 }
 function todolistdelete(ele){
   ele.parentElement.remove()
 }
+function todolistEdit(ele){
+  
+  let todolisteditenable = document.querySelector('.to-do-list-input')
+  if(todolisteditenable.style.visibility == 'visible'){
+    todolisteditenable.style.visibility = 'hidden'
+    ele.innerHTML = `Edit  <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+                      <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
+                          stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                        <path
+                          d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
+                          stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                      </svg>`
+    todolist_delete_button_two()
+  }
+  else{
+    todolisteditenable.style.visibility = 'visible'
+    ele.innerText = 'save'
+    todolist_delete_button_one()
+  }
+}
+let todolistdeletebutton = document.getElementsByClassName('todolist-delete-button')
+function todolist_delete_button_one(){
+  
+  for(let x = 0; x<todolistdeletebutton.length; x++){
+      todolistdeletebutton[x].style.display = 'block'
+  }
+}
+function todolist_delete_button_two(){
+  
+  for(let x = 0; x<todolistdeletebutton.length; x++){
+      todolistdeletebutton[x].style.display = 'none'
+  }
+}
+
+let todolistupbutton = document.getElementById('todolist-up-button')
+let todolist_overflow_box = document.querySelector('.to-do-list-edit-main-page-body-inner')
+let todolistdownbutton = document.getElementById('todolist-down-button');
+todolist_overflow_box.addEventListener('scroll', ()=>{
+  console.log(todolist_overflow_box.scrollHeight)
+ 
+  if(todolist_overflow_box.scrollTop > 0){
+    todolistupbutton.style.display = 'block'
+  }
+  else{
+    todolistupbutton.style.display = 'none'
+  }
+
+  if(todolist_overflow_box.scrollTop+todolist_overflow_box.scrollHeight ==  todolist_overflow_box.scrollHeight){
+    todolistdownbutton.style.display = 'block'
+  }
+  else{
+    todolistdownbutton.style.display = 'none'
+  }
+})
+todolistupbutton.addEventListener('click', ()=>{
+   todolist_overflow_box.scrollTop =  0;
+
+})
+todolistdownbutton.addEventListener('click',()=>{
+   todolist_overflow_box.scrollTop =  todolist_overflow_box.scrollHeight
+})
